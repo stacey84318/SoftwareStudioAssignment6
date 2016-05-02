59915b6659a45735794dac6b61bf762e65f3e408
stacey84318 <stacey84318@yahoo.com.tw>
2016-05-02 22:10:24 +0800

    add load data_stacey

--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
@@ -14,10 +15,18 @@ import processing.event.MouseEvent;
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
 	private String path = "main/resources/";
-	private String file = "starwars-episode-1-interactions.json";
 	JSONObject data;
 	JSONArray nodes, links;
 	private ArrayList<Character> characters;
+	private String file1 = "starwars-episode-1-interactions.json";
+	private String file2 = "starwars-episode-2-interactions.json";
+	private String file3 = "starwars-episode-3-interactions.json";
+	private String file4 = "starwars-episode-4-interactions.json";
+	private String file5 = "starwars-episode-5-interactions.json";
+	private String file6 = "starwars-episode-5-interactions.json";
+	private String file7 = "starwars-episode-5-interactions.json";
+
+	int episode=0;
 	
 	private final static int width = 1200, height = 650;
 	
@@ -40,6 +49,34 @@ public class MainApplet extends PApplet{
 	public void mouseDragged(MouseEvent e) {
 	    
 	}
+	 public void keyPressed(KeyEvent e){
+	        switch(e.getKeyCode()){
+	        case KeyEvent.VK_1: 
+	        	episode = 1;
+	        	break;
+	        case KeyEvent.VK_2: 
+	        	episode = 2;
+	        	break;
+	        case KeyEvent.VK_3: 
+	        	episode = 3;
+	        	break;
+	        case KeyEvent.VK_4: 
+	        	episode = 4;
+	        	break;
+	        case KeyEvent.VK_5: 
+	        	episode = 5;
+	        	break;
+	        case KeyEvent.VK_6: 
+	        	episode = 6;
+	        	break;
+	        case KeyEvent.VK_7: 
+	        	episode = 7;
+	        	break;
+	        default: 
+	        	episode = 1;
+	        	break;
+	        }
+	       }
 
 	public void draw() {
 		this.background(255);
@@ -48,6 +85,18 @@ public class MainApplet extends PApplet{
 	}
 
 	private void loadData(){
+		if(episode!=0)
+			this.data = loadJSONObject("file"+episode);
+		this.nodes = data.getJSONArray("nodes");
+		this.links = data.getJSONArray("links");
+		for(int i = 0 ;i<nodes.size();i++){
+			JSONObject N = nodes.getJSONObject(i);
+			int value = N.getInt("value");
+			String name = N.getString("name");
+			String color = N.getString("colour");
+			System.out.println(name+","+value+","+color);
+			this.characters.add(new Character(this,name,(i/10)*20,i*10));;
+		}
 
 	}
 
